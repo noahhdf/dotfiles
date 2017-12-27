9eba0598a7dac54e47e0536d5411ad77a09fa3db
Noah Biederbeck <noah.biederbeck@udo.edu>
2017-12-27 07:42:18 +0100

    new file to show all colors

--- colorschemes/colors.sh
+++ colorschemes/colors.sh
@@ -1,1 +1,10 @@
-/home/noah/shellScripts/colors.sh
+#!/bin/bash
+echo -e "\033[0mforeground"
+echo -e "\033[0;30mcolor0 (black)\t\033[1;30mcolor8 (bold_black)"
+echo -e "\033[0;31mcolor1 (red)\t\033[1;31mcolor9 (bold_red)"
+echo -e "\033[0;32mcolor2 (green)\t\033[1;32mcolor10 (bold_green)"
+echo -e "\033[0;33mcolor3 (yellow)\t\033[1;33mcolor11 (bold_yellow)"
+echo -e "\033[0;34mcolor4 (blue)\t\033[1;34mcolor12 (bold_blue)"
+echo -e "\033[0;35mcolor5 (purple)\t\033[1;35mcolor13 (bold_purple)"
+echo -e "\033[0;36mcolor6 (cyan)\t\033[1;36mcolor14 (bold_cyan)"
+echo -e "\033[0;37mcolor7 (white)\t\033[1;37mcolor15 (bold_white)"
