df8db06d16d38c6ce9f7704c195c4d57b9a5bf6d
Noah Biederbeck <noah.biederbeck@udo.edu>
2019-07-03 01:36:15 +0200

    again use two bars

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -4,8 +4,8 @@ killall -q polybar
 
 while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
 
-# polybar top &!
-# polybar bottom &!
-polybar combined &!
+polybar top &!
+polybar bottom &!
+# polybar combined &!
 
 echo "Bars launched"
